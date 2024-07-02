b75fe62fd79c5f29f43b1062aa6350f43a586545
Hugo <hugo.boudry@agorinfo.fr>
2024-07-02 14:31:46 +0200

    Adding permissions

---
+++ Flagdle/game/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework.permissions import BasePermission
+
+
+class UserPermissions(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ('GET', 'PUT', 'PATCH', 'OPTIONS', 'HEAD'):
+        # Only gives access to users while restricting them
+        return True
+        return request.user
+
+
+class IsAdminAuthenticated(BasePermission):
+    def has_permission(self, request, view):
+        # Only gives access to authenticated administrator users
+        return bool(request.user and request.user.is_superuser)
