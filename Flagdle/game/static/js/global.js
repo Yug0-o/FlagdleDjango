f7ded607d3ab74500f8199f6f77e07617f54e4e4
Yug0-o <hugoboudry@outlook.fr>
2024-07-17 15:41:39 +0200

    disabling right-click on game images

--- Flagdle/game/static/js/global.js
+++ Flagdle/game/static/js/global.js
@@ -75,4 +75,10 @@ window.onload = function () {
 
     // Reset score (conditional call)
     resetScore();
+
+    $('img').mousedown(function (e) {
+      if(e.button == 2) { // right click
+        return false; // do nothing!
+        }
+    });
 };
